0badcf86bcf43c7f4c4303176fbd20a1574725a2
cloux <cloux@rote.ch>
2019-06-08 22:01:56 +0200

    fix: whitelist "login" and "registration" in the domain

--- linksanitizer.user.js
+++ linksanitizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Link Sanitizer
 // @description  Clean up unnecessary hyperlink redirections and link shims
-// @version      1.1.4
+// @version      1.1.5
 // @author       cloux <cloux@rote.ch>
 // @license      WTFPL 2.0; http://www.wtfpl.net/about/
 // @namespace    https://github.com/cloux
@@ -28,7 +28,7 @@
 		// whitelisted services
 		if (/google\.[a-z]*\/ServiceLogin/.test(weblink) ||                      // google login service
 		    /^https:\/\/translate\.google\./.test(weblink) ||                    // Google translator
-		    /^http.*(login|registration)[/?].*http/.test(weblink) ||             // heise.de
+		    /^http.*(login|registration)[./?].*http/.test(weblink) ||            // aliexpress, heise.de
 		    /\/oauth\?/.test(weblink) ||                                         // OAuth on aws.amazon.com
 		    /\/signin\?openid/.test(weblink) ||                                  // amazon.com
 		    /^https?:\/\/downloads\.sourceforge\.net\//.test(weblink) ||         // downloads.sourceforge.net
